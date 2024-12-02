1117db767fc3740c0b2bc955709b953fdc3adac1
csm1717 <marnecharu10@gmail.com>
2024-12-02 19:07:12 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'csm',
+    author_email= 'csm@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
