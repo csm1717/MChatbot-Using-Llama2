7990ac1d8b48f3f641a3e233f89109247b6c3241
csm1717 <marnecharu10@gmail.com>
2024-12-03 12:11:47 +0530

    templates added

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template , jsonify , request
+from src.helper import download_hugging_face_embeddings
+from langchain_community.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_community.llms import ctransformers
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+
+# load_dotenv()
+
+# PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+# PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# embeddings = download_hugging_face_embeddings()
+
+# # initializing the pinecone 
+# pinecone.init(api_key = PINECONE_API_KEY,
+#               environment = PINECONE_API_ENV)
+
+# index_name = "medical-chatbot"
+
+
+# #Creating Embeddings for Each of The Text Chunks & storing
+# docsearch=Pinecone.from_existing_index(index_name, embeddings)
+
+# PROMPT = PromptTemplate(template=prompt_template, input_variables= ["context", "questions"])
+
+# chain_type_kwargs = {"prompt" : PROMPT}
+
+# llm= ctransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+#                   model_type="llama",
+#                   config={'max_new_tokens':512,
+#                           'temperature':0.8})
+
+# qa=RetrievalQA.from_chain_type(
+#     llm=llm, 
+#     chain_type="stuff", 
+#     retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+#     return_source_documents=True, 
+#     chain_type_kwargs=chain_type_kwargs)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port= 8080, debug=True)
